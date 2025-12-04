94c21f608579cf91a83d76ddc854b3295d72adee
Aaron Espinosa <aaron@esencial.work>
2025-12-03 20:36:47 -0600

    Refactor: Move Hub to root and HTML Demo to public/demo-boda

---
+++ plasmar-landing/.next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_b53ad65d._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Proyectos WIP/Save The Date_Wedding/plasmar-landing/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/4c30b_77ba1979._.js",
+  "chunks/[root-of-the-server]__88a27080._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Proyectos WIP/Save The Date_Wedding/plasmar-landing/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
